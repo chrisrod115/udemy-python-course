77f9d0ab6daea3f8203c53f3e8a1ef957fefb51b
Christopher Rodriguez <chrisrod115@gmail.com>
2023-11-25 22:27:27 -0800

    Day 1 complete, added two files from the lectures.

---
+++ 100DaysOfCode/Day_1_BandNameGenerator/interactive_code/string_count.py
@@ -0,0 +1,16 @@
+"""METHOD 1: Using len() function
+"""
+# str_length = len(input("Enter your name: "))
+# print(str_length)
+
+"""METHOD 2: Using for loop
+"""
+# str_length = 0
+# for letter in input("Enter your name: "):
+#     str_length += 1
+# print(str_length)
+
+"""METHOD 3: Using count() function
+"""
+# str_length = input("Enter your name: ").count("")
+# print(str_length - 1)
