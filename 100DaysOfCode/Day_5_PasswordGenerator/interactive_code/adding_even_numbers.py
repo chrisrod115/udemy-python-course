ffad1ab1d83131cbee77b0ed605b2d9808a16ee3
Christopher Rodriguez <62278746+chrisrod115@users.noreply.github.com>
2023-11-30 17:43:06 -0800

    updated missing files.

---
+++ 100DaysOfCode/Day_5_PasswordGenerator/interactive_code/adding_even_numbers.py
@@ -0,0 +1,10 @@
+target = int(input()) # Enter a number between 0 and 1000
+# 🚨 Do not change the code above ☝️
+
+# Write your code here 👇
+even_total = 0
+for i in range(1, target + 1):
+    if (i % 2 == 0):
+        even_total += i
+
+print(even_total)
---
+++ 100DaysOfCode/Day_5_PasswordGenerator/interactive_code/high_score.py
@@ -0,0 +1,14 @@
+# Input a list of student scores
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+
+# Write your code below this row 👇
+
+highest = 0
+
+for score in student_scores:
+    if score > highest:
+        highest = score
+
+print(f"The highest score in the class is: {highest} ")
