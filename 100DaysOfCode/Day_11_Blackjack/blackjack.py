4616146ec839ea3a91631c06102ba5454922cf1a
Christopher Rodriguez <chrisrod115@gmail.com>
2023-12-08 12:50:44 -0800

    added updated blackjack game.

--- 100DaysOfCode/Day_11_Blackjack/blackjack.py
+++ 100DaysOfCode/Day_11_Blackjack/blackjack.py
@@ -1,52 +1,73 @@
-import random as rand
-face_card_dict = {
-    "A": [1, 11],
-    "K": 10,
-    "Q": 10,
-    "J": 10,
-}
-
-non_face_card = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-
+import random
+# face_card_dict = {
+#     "A": [1, 11],
+#     "K": 10,
+#     "Q": 10,
+#     "J": 10,
+# }
 
 def deal_cards():
-    first = rand.choice(non_face_card)
-    second = rand.choice(non_face_card)
-    return[first, second]
-
+    return [random.choice(cards), random.choice(cards)]
 
 def add_card():
-    return rand.choice(non_face_card)
+    return random.choice(cards)
+
+def dealer_hit_16(dealer_hand):
+    while sum_hand(dealer_hand) < 17:
+        dealer_hand.append(add_card())
+    return dealer_hand
+
+def sum_hand(hand):
+    total = sum(hand)
+    aces_count = hand.count(11)
+
+    while total > 21 and aces_count:
+        total -= 10 
+        aces_count -= 1
+
+    return total
 
 
-def dealer_hit_16(dealer):
-    hit_16_dealer_list = dealer
-    dealer_total = sum(dealer)
-    if dealer_total < 17:
-        hit_16_dealer_list.append(add_card())
+def is_bust(hand):
+    return sum_hand(hand) > 21
+
+def play_blackjack():
+    player_hand = deal_cards()
+    dealer_hand = deal_cards()
+    print(f"Your Cards: {player_hand}")
+    print(f"Dealer's Cards: [{dealer_hand[0]}, Hidden]")
+
+    while True:
+        if sum_hand(player_hand) == 21:
+            return "Blackjack! You win!"
+
+        choice = input("Type 'hit' or 'stay': ").lower()
+        if choice == 'hit':
+            player_hand.append(add_card())
+            print(f"Your Cards: {player_hand}")
+            if is_bust(player_hand):
+                return "Busted! Sorry, you lost."
+        else:
+            break
+
+    print(f"Dealer's Cards: {dealer_hand}")
+    dealer_hand = dealer_hit_16(dealer_hand)
+    if is_bust(dealer_hand):
+        return f"Dealer busts. You win! Your hand: {player_hand}"
+
+    if sum_hand(player_hand) > sum_hand(dealer_hand):
+        return f"You win! Your hand: {player_hand}, Dealer's hand: {dealer_hand}"
+    elif sum_hand(player_hand) == sum_hand(dealer_hand):
+        return f"Draw! Your hand: {player_hand}, Dealer's hand: {dealer_hand}"
     else:
-        return hit_16_dealer_list
-    
+        return f"You lost! Your hand: {player_hand}, Dealer's hand: {dealer_hand}"
 
-def play_bj():
-    player_cards = deal_cards()
-    print(f"Your Cards: {player_cards}")
-    dealer_cards = deal_cards()
-    print(f"Dealer Cards: {dealer_cards}")
-    hit_or_stay = str(input("Type either, 'hit' or 'stay': ")).strip().lower()
-    if hit_or_stay == "hit": 
-        add_card()
-    else:
-        dealer_hit_16()
-        
-        
+# cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]  # 10s and face cards are all worth 10, 11 for Aces
+cards = [11, 11]  # 10s and face cards are all worth 10, 11 for Aces
+
+
 print("Welcome to Blackjack!")
-playing = True
-while playing: 
-    
-    play_bj()
-    play_again = str(input("Would you like to play again 'y' or 'n': ")).strip().lower()
-    if play_again == "y":
-        continue
-    else:
-        playing = False
+while True:
+    print(play_blackjack())
+    if input("Play again? (y/n): ").lower() != 'y':
+        break
