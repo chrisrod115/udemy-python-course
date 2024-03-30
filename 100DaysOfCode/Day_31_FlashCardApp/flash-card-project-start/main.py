255dc9d9a275fd2a3f0bfb650ccd13e369aa10c5
Christopher Rodriguez <chrisrod115@gmail.com>
2024-03-30 15:53:18 -0700

    update
    Commit 1

--- 100DaysOfCode/Day_31_FlashCardApp/flash-card-project-start/main.py
+++ 100DaysOfCode/Day_31_FlashCardApp/flash-card-project-start/main.py
@@ -1,54 +1,76 @@
+import random
 from tkinter import *
 import pandas as pd
 import time
+import os
+
+# Get the directory of the script
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
 BACKGROUND_COLOR = "#E7E8D1"
 FONT_COLOR_1 = "#A7BEAE"
 FONT_COLOR_2 = "#B85042"
 LANGUAGE_FONT = ("Courier", 30, "italic")
+FRENCH_WORD = os.path.join(script_dir, "data", "french_words.csv")
 
 WINDOW_SIZE_X = 900
 WINDOW_SIZE_Y = 700
 
-# Keep score/metrics
+# ------------------------ IMPORT DICTIONARY -------------------------- #
+word_list_csv = pd.read_csv(FRENCH_WORD)
+rand_num = random.randint(0, 99)
 
-# Checker for right or wrong answers
+def random_word_generator(trigger):
+    eng_word = word_list_csv["English"][rand_num]
+    fre_word = word_list_csv["French"][rand_num]
+    return eng_word, fre_word
 
-# Card flip logic
+# -------------------------- INITIALIZATION --------------------------- #
+START_APP = (1, 1)
 
-# Create the timer to flip cards
 
-# Design UI interface
+# --------------------------- FLIP LOGIC ------------------------------ #
+
+# --------------------------- TIMER LOGIC ----------------------------- #
+
+# -------------------------- BUTTON LOGIC ----------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.configure(background=BACKGROUND_COLOR)
-window.minsize(height=WINDOW_SIZE_Y,width=WINDOW_SIZE_X)
+window.minsize(height=WINDOW_SIZE_Y, width=WINDOW_SIZE_X)
 window.title("Flash Cards App")
 
 # Canvas
-flash_card_pic_front = PhotoImage(file="./images/card_front.png")
-flash_card_canvas_front = Canvas(height=550, width=810, bg=BACKGROUND_COLOR,borderwidth=0, highlightthickness=0)
-flash_card_canvas_front.create_image(405, 270, image=flash_card_pic_front)
+## Flash card front side
+flash_card_pic_front = PhotoImage(file=os.path.join(script_dir, "images", "card_front.png"))
+flash_card_canvas_front = Canvas(height=526, width=800, bg=BACKGROUND_COLOR, borderwidth=0, highlightthickness=0)
+flash_card_canvas_front.create_image(400, 263, image=flash_card_pic_front)
 flash_card_canvas_front.grid(column=0, row=0)
-flash_card_canvas_front.create_text(200, 100, text="Eng", font=LANGUAGE_FONT, fill="blue")
 
-flash_card_pic_back = PhotoImage(file="./images/card_back.png")
-flash_card_canvas_back = Canvas(height=550, width=810, bg=BACKGROUND_COLOR,borderwidth=0, highlightthickness=0)
+## Flash card back side
+flash_card_pic_back = PhotoImage(file=os.path.join(script_dir, "images", "card_back.png"))
+flash_card_canvas_back = Canvas(height=550, width=810, bg=BACKGROUND_COLOR, borderwidth=0, highlightthickness=0)
 flash_card_canvas_back.create_image(405, 270, image=flash_card_pic_back)
 flash_card_canvas_back.grid(column=1, row=1)
 
 # Labels
-en_language_label = Label(text="English", font=LANGUAGE_FONT)
-en_language_label.grid(column=0, row=1)
-fr_language_label = Label(text="French", font=LANGUAGE_FONT)
-fr_language_label.grid(column=0, row=0)
+## English word
+en_language_label = Label(text="eng_word", font=LANGUAGE_FONT)
+en_language_label.grid(column=1, row=1)
+## French word
+fr_language_label = Label(text="fre_word", font=LANGUAGE_FONT)
+fr_language_label.grid(column=1, row=1)
 
 # Buttons
-wrong_button_pic = PhotoImage(file="./images/wrong.png")
+## Red incorrect button
+wrong_button_pic = PhotoImage(file=os.path.join(script_dir, "images", "wrong.png"))
 wrong_button = Button(image=wrong_button_pic, bg=BACKGROUND_COLOR, borderwidth=0, highlightthickness=0)
-wrong_button.grid(column=2, row=2)
+wrong_button.grid(column=1, row=3)
 
-right_button_pic = PhotoImage(file="./images/right.png")
+## Green correct button
+right_button_pic = PhotoImage(file=os.path.join(script_dir, "images", "right.png"))
 right_button = Button(image=right_button_pic, borderwidth=0, highlightthickness=0, bg=BACKGROUND_COLOR)
-right_button.grid(column=3, row=3)
+right_button.grid(column=2, row=3)
 
 window.mainloop()
