9275b789d6a27ceec507eb266249dee61e8b4459
Christopher Rodriguez <62278746+chrisrod115@users.noreply.github.com>
2025-02-02 14:52:39 -0800

    Reworked caeser_cipher.

--- 100DaysOfCode/Day_008_CaeserCipher/caeser_cipher.py
+++ 100DaysOfCode/Day_008_CaeserCipher/caeser_cipher.py
@@ -1,43 +1,28 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-alphabet_list = list(alphabet)
-alphabet_list_length = len(alphabet_list)
+import string
 
-def encode(message_to_encode_decode, secret_shift_number):
-    encoded_message = ""
-    for letter in message_to_encode_decode:
-        if letter in alphabet_list:
-            letter_index = alphabet_list.index(letter)
-            encoded_index = (letter_index + secret_shift_number) % alphabet_list_length
-            encoded_message += alphabet_list[encoded_index]
+ALPHABET=string.ascii_lowercase
+
+# encode/decode
+def encode_decode(message, offset, en_de):
+    res = "" 
+    if en_de == 'decode':
+        offset *= -1
+
+    for char in message:
+        char=char.lower()
+        if char in ALPHABET:
+            start_index= ALPHABET.find(char)
+            new_index=(start_index+offset)%26
+            res += ALPHABET[new_index]
         else:
-            encoded_message += letter
-    print(f"The encoded message is: {encoded_message}\n")
+            res+=char
+                
+    return res
 
-def decode(message_to_encode_decode, secret_shift_number):
-    decoded_message = ""
-    for letter in message_to_encode_decode:
-        if letter in alphabet_list:
-            letter_index = alphabet_list.index(letter)
-            decoded_index = (letter_index - secret_shift_number) % alphabet_list_length
-            decoded_message += alphabet_list[decoded_index]
-        else:
-            decoded_message += letter
-    print(f"The decoded message is: {decoded_message}\n")
+# main
+message = input("Enter your message: ")
+offset = int(input("Enter the offset: "))
+choice = input("Encode or Decode? (encode/decode): ").lower()
 
-cipher = True
-while cipher:
-    decode_or_encode = str(input("Type 'encode' to encrypt, type 'decode' to decrypt: ")).strip().lower()
-    message_to_encode_decode = str(input("Type your message: ")).strip().lower()
-    secret_shift_number = int(input("Type your shift number: "))
-    if decode_or_encode == "encode":
-        encode(message_to_encode_decode, secret_shift_number)
-    elif decode_or_encode == "decode":
-        decode(message_to_encode_decode, secret_shift_number)
-    else:
-        print("Not valid option.\n")
-        continue
-    continue_or_not = str(input("Type 'yes' if you want to go again. Otherwise type 'no'. ")).strip().lower()
-    if continue_or_not == "yes":
-        continue
-    else:
-        cipher = False
+result = encode_decode(message, offset, choice)
+print("Result:", result)
