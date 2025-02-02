14eb6aa5d3727034e5a5c72bd525680d34e90fd5
Christopher Rodriguez <62278746+chrisrod115@users.noreply.github.com>
2025-02-02 09:34:25 -0800

    Reworked the password generator.

--- 100DaysOfCode/Day_005_PasswordGenerator/password_generator.py
+++ 100DaysOfCode/Day_005_PasswordGenerator/password_generator.py
@@ -1,17 +1,43 @@
-import random
+import random as r
+import os
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-symbols = "!@#$%^&*()_+"
 numbers = "1234567890"
+symbols = "!@#$%^&*()_+"
+
+clear_screen = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+
+def get_positive_integer_input(prompt):
+    while True:
+        try:
+            clear_screen()
+            value = int(input(prompt))
+            if value <= 0:
+                print("Please enter a value greater than 0.")
+            else:
+                return value
+        except ValueError:
+            print("Invalid Input. Only enter an integer.")
+
+def password_generator(letters_count, numbers_count, symbols_count):
+    password_characters = []  
+    
+    for _ in range(letters_count):
+        password_characters.append(r.choice(alphabet))
+    for _ in range(numbers_count):
+        password_characters.append(r.choice(numbers))
+    for _ in range(symbols_count):
+        password_characters.append(r.choice(symbols))
+
+    r.shuffle(password_characters)  
+    return "".join(password_characters)  
+
+
 print("Welcome to the PyPassword Generator!")
-number_of_letters = int(input("How many letters would you like in your password?\n"))
-number_of_symbols = int(input(f"How many symbols would you like?\n"))
-number_of_numbers = int(input(f"How many numbers would you like?\n"))
-password = ""
-for i in range(0, number_of_letters):
-    password += random.choice(alphabet)
-for i in range(0, number_of_symbols):
-    password += random.choice(symbols)
-for i in range(0, number_of_numbers):
-    password += random.choice(numbers)
-password = ''.join(random.sample(password, len(password)))
-print(f"Your password is: {password}")
+number_of_letters = get_positive_integer_input("How many letters would you like in your password?\n")
+number_of_symbols = get_positive_integer_input("How many symbols would you like?\n")
+number_of_numbers = get_positive_integer_input("How many numbers would you like?\n")
+
+clear_screen()
+password = password_generator(number_of_letters, number_of_symbols, number_of_numbers)
+print(f"Your generated password is: {password}") 
