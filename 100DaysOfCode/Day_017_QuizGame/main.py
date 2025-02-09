e929a32470b6e9d20e2e71db64008ee40d5bdd57
Christopher Rodriguez <62278746+chrisrod115@users.noreply.github.com>
2025-02-08 17:28:20 -0800

    Rework complete on update to quiz game (OOP).

--- 100DaysOfCode/Day_017_QuizGame/main.py
+++ 100DaysOfCode/Day_017_QuizGame/main.py
@@ -1,8 +1,12 @@
 from question_model import QuestionModel
 from data import question_data
 from quiz_brain import QuizBrain
+import os
 
 model=QuestionModel
+
+
+clear_screen=lambda:os.system('cls' if os.name=='nt' else 'clear')
 
 question_bank=[]
 for question in question_data:
@@ -12,4 +16,10 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz=QuizBrain(question_bank)
-print(quiz.get_question())
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+clear_screen()
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- 100DaysOfCode/Day_017_QuizGame/quiz_brain.py
+++ 100DaysOfCode/Day_017_QuizGame/quiz_brain.py
@@ -2,11 +2,25 @@ class QuizBrain:
     def __init__(self, list) -> None:
         self.question_number=0
         self.question_list=list
+        self.score=0
     
     
-    def get_question(self):
-        user_input=str(input(f"Q{self.question_number+1}: {self.question_list[self.question_number].question} (True/False)?: ")).strip().lower()
-        return user_input
+    def next_question(self):
+        current_question=self.question_list[self.question_number]
+        self.question_number+=1
+        user_answer=str(input(f"Q{self.question_number}: {current_question.question} (True/False)?: ")).strip().lower()
+        self.check_answer(user_answer, current_question.answer)
+        
     
     
-    def 
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    
+    def check_answer(self, user_answer, current_answer):
+        if (user_answer==current_answer.lower()):
+            print("You got that right!")
+            self.score+=1
+        else:
+            print("Sorry that was the wrong guess!")
+        print(f"Your current score is: {self.score}/{self.question_number}\n")
